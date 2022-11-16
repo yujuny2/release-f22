38511c9d548b11b531af7f4a314c489298c018b2
y-yujun <y.yujun178@gmail.com>
2022-11-16 19:07:01 +0000

    Completed mp_schedule

--- mp_schedule/src/schedule.cpp
+++ mp_schedule/src/schedule.cpp
@@ -25,7 +25,23 @@
  */
 V2D file_to_V2D(const std::string & filename){
     // Your code here!
+    // get all text data from file
+    V2D data;
+    std::string data_str = file_to_string(filename);
 
+    std::vector<std::string> row_data;
+    int num_rows = SplitString(data_str, '\n', row_data);
+
+    for (int i = 0; i < num_rows; i++) {
+        std::vector<std::string> data_vec;
+        int num_cols = SplitString(row_data[i], ',', data_vec);
+        for (int j = 0; j < num_cols; j++) {
+            data_vec[j] = Trim(data_vec[j]);
+        }
+        data.push_back(data_vec);
+    }
+
+    return data;
 }
 
 /**
@@ -40,7 +56,49 @@ V2D file_to_V2D(const std::string & filename){
  */
 V2D clean(const V2D & cv, const V2D & student){
     // YOUR CODE HERE
+    V2D cleaned_data;
 
+    std::unordered_map<std::string, std::vector<std::string>> courses;
+    std::unordered_map<std::string, std::vector<std::string>> students;
+    std::unordered_map<std::string, std::vector<std::string>> roster;
+    std::vector<std::string> order;
+
+    // get courses map
+    for (unsigned i = 0; i < cv.size(); i++) {
+        std::vector<std::string> c = cv[i];
+        std::string course_name = c[0];
+        std::vector<std::string> students_name = {c.begin() + 1, c.end()};
+        courses[course_name] = students_name;
+        order.push_back(course_name);
+    }
+
+    // get students map
+    for (unsigned i = 0; i < student.size(); i++) {
+        std::vector<std::string> s = student[i];
+        std::string student_name = s[0];
+        std::vector<std::string> courses_name = {s.begin() + 1, s.end()};
+        students[student_name] = courses_name;
+    }
+
+    // check courses map
+    for (std::pair<std::string, std::vector<std::string>> key_val : courses) {
+        roster[key_val.first] = {key_val.first};
+
+        for (unsigned j = 0; j < key_val.second.size(); j++) { // loop through students in a course
+            std::vector<std::string> c = students[key_val.second[j]];
+            if (std::find(c.begin(), c.end(), key_val.first) != c.end()) {
+                roster[key_val.first].push_back(key_val.second[j]);
+            }
+        }
+    }
+
+    for (unsigned i = 0; i < order.size(); i++) {
+        if (roster[order[i]].size() > 1) {
+            cleaned_data.push_back(roster[order[i]]);
+        }
+    }
+
+    return cleaned_data;
 }
 
 /**
@@ -61,5 +119,93 @@ V2D clean(const V2D & cv, const V2D & student){
  */
 V2D schedule(const V2D &courses, const std::vector<std::string> &timeslots){
     // Your code here!
+    V2D new_schedule;
+    unsigned num_courses = courses.size();
 
+    // adjacency matrix that represents the edges
+    std::vector<std::vector<bool>> edges(num_courses, std::vector<bool>(num_courses, false));
+
+    for (unsigned i = 0; i < num_courses; i++) {
+        for (unsigned j = 1; j < courses[i].size(); j++) {
+            for (unsigned k = i + 1; k < num_courses; k++) {
+                if (std::find(courses[k].begin(), courses[k].end(), courses[i][j]) != courses[k].end()) {
+                    edges[i][k] = true;
+                    edges[k][i] = true;
+                }
+            }
+        }
+    } 
+
+    // get course names
+    std::vector<std::string> course_names;
+    for (unsigned i = 0; i < num_courses; i++) {
+        course_names.push_back(courses[i][0]);
+    }
+
+    // graph coloring
+    bool done = true;
+    for (unsigned i = 0; i < num_courses; i++) { // loop through each starting vertex
+        std::unordered_map<int, std::vector<std::string>> map;
+        map[0] = {timeslots[0], course_names[0]};
+        std::vector<int> colors = {0};
+
+        for (unsigned j = 1; j < num_courses; j++) { // loop through vertices
+            int color = 0;
+            std::vector<bool> available(map.size(), false);
+            std::vector<bool> unavailable(map.size(), false);
+
+            for (unsigned k = 0; k < j; k++) { // loop through visited vertices
+                if (!edges[j][k]) {
+                    if (!unavailable[colors[k]]) {
+                        available[colors[k]] = true;
+                    }
+                } else {
+                    // check if the color of this edge exists in the available color vector, if exists, remove the color
+                    unavailable[colors[k]] = true;
+                    if (available[colors[k]]) {
+                        available[colors[k]] = false;
+                    }
+                }
+            }
+
+            for (unsigned k = 0; k < available.size(); k++) {
+                if (available[k]) {
+                    color = colors[k];
+                    break;
+                }
+                color = k + 1;
+            }
+
+            // if the number of colors is greater than the number of timeslots, go to the next option
+            if (color >= int(timeslots.size())) {
+                done = false;
+                break;
+            }
+
+            std::unordered_map<int, std::vector<std::string>>::iterator lookup = map.find(color);
+            if (lookup != map.end()) {
+                map[color].push_back(course_names[j]);
+            } else {
+                map[color] = {timeslots[color], course_names[j]};
+            }
+            colors.push_back(color);
+        }
+
+
+        // terminate if there is valid labeling
+        if (map.size() == timeslots.size() && done) {
+            for (std::pair<int, std::vector<std::string>> key_val : map) {
+                new_schedule.push_back(key_val.second);
+            }
+            return new_schedule;
+        }
+
+        // start from the second vertex if the current vertex fails; move the first vertex to the last
+        std::rotate(course_names.begin(), course_names.begin() + 1, course_names.end());
+        std::rotate(edges.begin(), edges.begin() + 1, edges.end());
+        for (unsigned i = 0; i < num_courses; i++) {
+            std::rotate(edges[i].begin(), edges[i].begin() + 1, edges[i].end());
+        }
+    }
+    return {{"-1"}};
 }
